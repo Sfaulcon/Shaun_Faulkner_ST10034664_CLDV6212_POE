a95288f86fda074c1798404f22d71b7a392914e6
Sfaulcon <shaun.faulkner.19@gmail.com>
2024-08-30 13:35:04 +0200

    Add project files.

---
+++ Shaun_Faulkner_ST10034664_CLDV6212_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shaun_Faulkner_ST10034664_CLDV6212_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
